dda95c68357bb1aa074c902ec95a72718f4c51c6
pmccabe5 <36641500+pmccabe5@users.noreply.github.com>
2020-12-13 20:36:47 -0500

    Update and rename packages.sh to ubuntu-setup.sh

---
+++ Environment-Setup/ubuntu-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# update and install necessary packages
+sudo apt update
+sudo apt install ssh net-tools git python python3 ansible
+
+# start ssh client and enable the 
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+# add devops user as a non-root user
+sudo useradd -m  -s /bin/bash devops
+passwd devops
+
+# add devops to the sudo group
+sudo usermod -aG devops sudo
+
+# switch users to devops
+su devops
+
+# generate ssh key
+ssh-keygen
